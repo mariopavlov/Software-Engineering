bca1c0483026ed92d6649b04e0708e1739dc08e4
Mario Pavlov <marioppavlov@gmail.com>
2021-05-16 21:48:34 +0300

    Refactoring a little bit the structure of the repository, and adding Linked List implementations

---
+++ Concepts/Data Structures/Linked List/linked_list.py
@@ -0,0 +1,156 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def to_list(self):
+        out = []
+        node = self.head
+        while node:
+            out.append(node.value)
+            node = node.next
+
+        return out
+
+    def create_from_list(self, python_list=None):
+        """ Create linked list from Python List """
+        self.head = None
+        if python_list:
+            for value in python_list:
+                self.append(value)
+
+    def prepend(self, value):
+        """ Prepend a value to the beginning of the list.
+        @param value: expects integer value
+        """
+
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            new_element = Node(value)
+            new_element.next = self.head
+            self.head = new_element
+
+    def append(self, value):
+        """ Append new node in the end of a Linked List
+
+        @param value: it can be either integer value, or LinkedList node
+        """
+
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            # Iterate over the linked list until we have the tail
+            tail = self.head
+            while tail.next is not None:
+                tail = tail.next
+
+            # Append the new node at the end of current Linked list
+            tail.next = Node(value)
+
+    def search(self, value):
+        """ Search the linked list for a node with the requested value and return the node. """
+        # I will perform a linear search, not like I have too many options
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.value == value:
+                return current_node
+
+    def remove(self, value):
+        """ Remove first occurrence of value. """
+
+        if self.head is not None:
+            # Check if I need to remove the head value
+            if self.head.value == value:
+                # If there is only one element in the list
+                if self.head.next is None:
+                    self.head = None
+                    return
+
+                # Remove head
+                self.head = self.head.next
+                return
+
+            # Remove element that is not head
+            current_node = self.head
+            next_node = current_node.next
+
+            # Iterate over the list until element is found, or list end is reached
+            while next_node is not None:
+                if next_node.value == value:
+                    # Remove one element
+                    # I need to perform it that way because of not having back-link
+                    current_node.next = next_node.next
+                    return
+
+                # Get next elements in the List
+                current_node = current_node.next
+                next_node = current_node.next
+
+    def pop(self):
+        """ Return the first node's value and remove it from the list. """
+        if self.head is not None:
+            popped_element = self.head
+
+            if self.head.next is None:
+                self.head = None
+
+            self.head = self.head.next
+            return popped_element.value
+
+        return None
+
+    def insert(self, value, pos):
+        """ Insert value at pos position(Zero based) in the list. If pos is larger than the
+        length of the list, append to the end of the list. """
+        current_position = 0
+
+        # Insert at position 0
+        if pos == current_position:
+            new_element = Node(value)
+            new_element.next = self.head
+            self.head = new_element
+            return
+
+        current_element = self.head
+        while current_element is not None:
+            current_position += 1
+            if pos == current_position:
+                new_element = Node(value)
+                new_element.next = current_element.next
+                current_element.next = new_element
+                return
+
+            if current_element.next is None:
+                current_element.next = Node(value)
+                return
+
+            current_element = current_element.next
+
+    def size(self):
+        """ Return the size or length of the linked list. """
+        list_size = 0
+        current_node = self.head
+
+        while current_node is not None:
+            list_size += 1
+            current_node = current_node.next
+
+        return list_size
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.value
+            node = node.next
+
+    def __repr__(self):
+        return str([v for v in self])
+
+
---
+++ Concepts/Data Structures/Linked List/linked_list_practice.py
@@ -0,0 +1,112 @@
+import linked_list as ll
+
+
+linked_list = ll.LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(1)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(3)
+
+print(f'List: {linked_list.to_list()}')
+
+linked_list.remove(1)
+print(f'Removing [1]: {linked_list.to_list()}')
+assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
+
+linked_list.remove(3)
+print(f'Removing [3]: {linked_list.to_list()}')
+assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+
+linked_list.remove(3)
+print(f'Removing [3]: {linked_list.to_list()}')
+assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
+
+linked_list.remove(3)
+print(f'Trying to remove element that is not in the list [3]: {linked_list.to_list()}')
+
+print(f'Final list: {linked_list.to_list()}')
+
+element = linked_list.pop()
+print(f'Pop first element! Value: {element}')
+print(f'List after popping first element: {linked_list.to_list()}')
+
+linked_list.insert(11, 0)
+print(f'Insert element at position 0: {linked_list.to_list()}')
+
+linked_list.insert(10, 13)
+print(f'Insert element at larger than list size pos [13]: {linked_list.to_list()}')
+
+linked_list.insert(99, 1)
+print(f'Insert element at [1]: {linked_list.to_list()}')
+
+print(f'List size is: {linked_list.size()}')
+
+
+def reverse(linked_list):
+    """ Returns reversed version of a list """
+    temp = ll.LinkedList()
+
+    for item in linked_list:
+        temp.prepend(item)
+
+    return temp
+
+
+reversed_list = reverse(linked_list)
+print(f'Reversed list: {reversed_list.to_list()}')
+
+
+def is_circular(linked_list):
+    """
+    Determine whether the Linked List is circular or not
+
+    Args:
+       linked_list(obj): Linked List to be checked
+    Returns:
+       bool: Return True if the linked list is circular, return False otherwise
+    """
+
+    if linked_list.head is None:
+        return False
+
+    fast_runner = linked_list.head
+    slow_runner = linked_list.head
+
+    while fast_runner.next is not None:
+        fast_runner = fast_runner.next.next
+        slow_runner = slow_runner.next
+
+        if slow_runner == fast_runner:
+            return True
+
+    return False
+
+
+# Test Cases
+list_with_loop = ll.LinkedList()
+list_with_loop.create_from_list([2, -1, 3, 0, 5])
+
+print(f'List after Init: {list_with_loop.to_list()}')
+
+# Creating a loop where the last node points back to the second node
+# loop_start is equal to the second node [-1]
+loop_start = list_with_loop.head.next
+
+# Iterate over the original list
+node = list_with_loop.head
+while node.next:
+    node = node.next
+
+# We have the last element in the list,
+# create the Loop by setting the last element to point to the second
+node.next = loop_start
+
+empty_linked = ll.LinkedList()
+empty_linked.create_from_list([])
+
+print(f'Circular Loop?: {is_circular(list_with_loop)}')
+print("Pass" if is_circular(list_with_loop) else "Fail")                  # Pass
+print("Pass" if is_circular(empty_linked) else "Fail")                  # Fail
+
