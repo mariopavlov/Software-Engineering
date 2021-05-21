d3cfea6b6cadbcc2e04af04a33e4dc6fdd58289f
Mario Pavlov <marioppavlov@gmail.com>
2021-05-21 07:56:03 +0300

    Refactoring
    - Changing the name of the nested list module

---
+++ data_structures/linked_list/src/nested_list/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from nested_list import NestedList
--- data_structures/linked_list/src/nested_list/nested_list.py
+++ data_structures/linked_list/src/nested_list/nested_list.py
@@ -1,8 +1,11 @@
 from ..linked_list import LinkedList
 
 
-class NestedLinkedList(LinkedList):
+class NestedList(LinkedList):
     """ Linked List it just like regular Linked List but every node can contain another Linked list """
 
     def flatten(self):
         """ """
+        result = LinkedList()
+
+        return result
