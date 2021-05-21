b54952da1affedd3a5fd860a262b0b403fee0acc
Mario Pavlov <marioppavlov@gmail.com>
2021-05-21 08:11:50 +0300

    Adding 2 more unit tests
    - 1 nested list
    - 2 nested lists

--- data_structures/linked_list/tests/test_nested_list.py
+++ data_structures/linked_list/tests/test_nested_list.py
@@ -22,6 +22,25 @@ class NestedListTests(unittest.TestCase):
 
         self.assertEqual(expected, empty_nested.to_list())
 
+    def test_1_nested_list(self):
+        """ Test if we only have 1 element, which is Linked List """
+        nested = NestedList()
+        list1 = LinkedList([1, 2, 3])
+        nested.append(list1)
+
+        expected = [1, 2, 3]
+        result = nested.flatten().to_list()
+
+        self.assertEqual(expected, result)
+
+    def test_2_nested_lists(self):
+        """ Test the most basic variant with 2 nested lists """
+
+        expected = [1, 2, 3, 4, 5, 6, 7, 8]
+        result = self.nested.flatten().to_list()
+
+        self.assertEqual(expected, result)
+
     def test_4_nested_lists(self):
         """ Extend basic NestedList with 2 more nested lists, and test the outcome """
         list3 = LinkedList([9])
@@ -31,7 +50,7 @@ class NestedListTests(unittest.TestCase):
         self.nested.append(list4)
 
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        result = self.nested.flatten()
+        result = self.nested.flatten().to_list()
 
         self.assertEqual(expected, result)
 
