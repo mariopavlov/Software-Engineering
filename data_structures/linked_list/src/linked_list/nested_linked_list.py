755ca59e05de7401479259181c500075b716b5d4
Mario Pavlov <marioppavlov@gmail.com>
2021-05-21 07:41:36 +0300

    Nested List Initial Class

---
+++ data_structures/linked_list/src/linked_list/nested_linked_list.py
@@ -0,0 +1,8 @@
+from linked_list import LinkedList
+
+
+class NestedLinkedList(LinkedList):
+    """ Linked List it just like regular Linked List but every node can contain another Linked list """
+
+    def flatten(self):
+        """ """
