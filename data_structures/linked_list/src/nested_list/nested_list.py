f2cb0a4ffbf9ccf332e9aeda0090b43da8c12845
Mario Pavlov <marioppavlov@gmail.com>
2021-05-21 08:36:35 +0300

    Implemented Flatten on Nested List

--- data_structures/linked_list/src/nested_list/nested_list.py
+++ data_structures/linked_list/src/nested_list/nested_list.py
@@ -6,10 +6,20 @@ class NestedList(LinkedList):
     """ Linked List it just like regular Linked List but every node can contain another Linked list """
 
     def flatten(self):
-        """ """
-        list1 = self.head.value
-        list2 = self.head.next.value
+        """ It takes N-numbered Nested List and returns single Linked List """
 
-        result = merge(list1, list2)
+        result = LinkedList
+
+        if self.head is not None:
+            if self.head.next is not None:
+                list1 = self.head.value
+                list2 = self.head.next.value
+
+                result = merge(list1, list2)
+                self.head.next.value = result
+                self.head = self.head.next
+                self.flatten()
+
+            result = self.head.value
 
         return result
