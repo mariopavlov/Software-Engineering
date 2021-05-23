e7094119906f538e2b6fb6481da10096cc53cd1e
Mario Pavlov <marioppavlov@gmail.com>
2021-05-23 21:24:12 +0300

    Initial Files for add one problem

---
+++ data_structures/list/src/list/utilities/add_one.py
@@ -0,0 +1,12 @@
+""" Incrementing List of digits with 1 (one) and return again List of digits """
+
+
+def add_one(arr):
+    """ Takes number, and returns the incremented result
+
+    @param arr: List of digits (representing number)
+    @return: List representing number
+    """
+    result = []
+
+    return result
