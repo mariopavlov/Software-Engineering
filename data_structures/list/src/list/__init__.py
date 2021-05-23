012b92f7985e45f9aac2563cd559a9c0a37f9aef
Mario Pavlov <marioppavlov@gmail.com>
2021-05-23 21:24:26 +0300

    Adding first unit test to check empty list

---
+++ data_structures/list/src/list/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .utilities.add_one import add_one
--- data_structures/list/tests/test_add_one.py
+++ data_structures/list/tests/test_add_one.py
@@ -1,8 +1,16 @@
 import unittest
+from data_structures.list.src.list import add_one
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class AddOneTests(unittest.TestCase):
+    def test_empty_list(self):
+        """ Test empty list to return empty list as result """
+        input_arr = []
+
+        result = add_one(input_arr)
+
+        self.assertEqual([], result)
+
 
 if __name__ == '__main__':
     unittest.main()
