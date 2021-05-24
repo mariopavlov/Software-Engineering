0fc9816ca6efe6fc42abf0602589573a0c59b2f4
Mario Pavlov <marioppavlov@gmail.com>
2021-05-24 09:26:07 +0300

    Implemented Solution to the problem

--- data_structures/list/src/list/utilities/duplicate_number.py
+++ data_structures/list/src/list/utilities/duplicate_number.py
@@ -6,4 +6,13 @@ def duplicate_number(arr):
     @param arr: Python list of numbers
     @return: None if no duplicate number is found, Integer if duplicate number is found
     """
+
+    temp = set()
+
+    for num in arr:
+        if num in temp:
+            return num
+        else:
+            temp.add(num)
+
     return None
