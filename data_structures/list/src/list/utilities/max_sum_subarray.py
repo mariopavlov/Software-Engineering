b8c115d109723c10db5bcef71c2bf943ef30c1f5
Mario Pavlov <marioppavlov@gmail.com>
2021-05-24 16:51:08 +0300

    Initial max_sum_subarray function body

---
+++ data_structures/list/src/list/utilities/max_sum_subarray.py
@@ -0,0 +1,10 @@
+
+
+def max_sum_subarray(arr):
+    """ Looking for maximum continuous sum inside list of numbers that is not separated with sub-array.
+    Sub-array is created when single element is higher than the current sum of the sub-array + element itself.
+
+    @param arr: List of numbers (negative and positive)
+    @return: Maximum sum inside the number that is not separated from Sub-array
+    """
+    pass
