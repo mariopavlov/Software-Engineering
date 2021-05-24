73ad95f422f97c0758602b197ccf8c051368ed41
Mario Pavlov <marioppavlov@gmail.com>
2021-05-24 09:18:11 +0300

    Adding duplicate_number function

---
+++ data_structures/list/src/list/utilities/duplicate_number.py
@@ -0,0 +1,9 @@
+
+
+def duplicate_number(arr):
+    """ Check List of Integer elements for duplicate number, and returns the value if found
+
+    @param arr: Python list of numbers
+    @return: None if no duplicate number is found, Integer if duplicate number is found
+    """
+    return None
