44fbc59c4513fedddb0e061bfc57d3f76583df95
Mario Pavlov <marioppavlov@gmail.com>
2021-05-25 22:22:41 +0300

    Partially implemented Pasca's Triangle - Handle negative numbers; - Return first two rows of the Pascal's Triangle;

--- data_structures/pascal_triangle/src/pascal_triangle/utilities/get_row.py
+++ data_structures/pascal_triangle/src/pascal_triangle/utilities/get_row.py
@@ -1,7 +1,17 @@
 
-def nth_row_pascal(n):
+def nth_row_pascal(row):
     """
-    :param: - n - index (0 based)
+    :param: - n - index (0 based), row that I need to return
     return - list() representing nth row of Pascal's triangle
     """
+
+    triangle = [[1], [1, 1]]
+
+    if row < 0:
+        print('Enter valid row number!')
+        return None
+
+    if row is 0 or row is 1:
+        return triangle[row]
+
     pass
