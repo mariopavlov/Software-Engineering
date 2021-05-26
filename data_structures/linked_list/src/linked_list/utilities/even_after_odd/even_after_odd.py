7b5b5a7a9e705c5a25aa53115bbb582cf4abedf0
Mario Pavlov <marioppavlov@gmail.com>
2021-05-26 20:11:08 +0300

    Even-After-Odd function definition

---
+++ data_structures/linked_list/src/linked_list/utilities/even_after_odd/even_after_odd.py
@@ -0,0 +1,7 @@
+
+def even_after_odd(head):
+    """ Inplace sorting of Linked List, to produce Even After Odd ordering
+    :param - head - head of linked list
+    return - updated list with all even elements are odd elements
+    """
+    pass
