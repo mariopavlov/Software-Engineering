a4cd78bd14dc5a8d38989ecbce71380be77222e8
Mario Pavlov <marioppavlov@gmail.com>
2021-05-30 10:22:38 +0300

    Fixing issues with Unit Tests and swap-nodes module

--- data_structures/linked_list/src/linked_list/utilities/swap_nodes.py
+++ data_structures/linked_list/src/linked_list/utilities/swap_nodes.py
@@ -1,3 +1,4 @@
+from ..linked_list import LinkedList
 
 
 def swap_nodes(link_list, left, right):
@@ -5,9 +6,10 @@ def swap_nodes(link_list, left, right):
     :param: link_list - pointer to LinkedList
     :param: `left` - indicates position (index) left
     :param: `right` - indicates position (index) right
-    return: None (Inplace changing of LinkedList elements)
+    return: True if list is changed or None
 
     TODO: complete this function and swap nodes present at position_one and position_two
     Do not create a new linked list
     """
-    pass
+
+    return None
